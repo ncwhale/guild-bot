85b435ecf04758fb0b98ee30f80673c3e97b6d66
Whale Mo <ncwhale@gmail.com>
2020-09-30 16:22:04 +0800

    Update config & http init.

--- config.template.js
+++ config.template.js
@@ -1,7 +1,8 @@
 const config = {
-    bot_token:"PUT_YOUR_BOT_TOKEN_HERE",
-    listen: "127.0.0.1:3000"
+    bot_token: "PUT_YOUR_BOT_TOKEN_HERE",
+    path: "./path/to/socket", // Use path for IPC listen
+    port: 3000,               // Or use port [host]
+    host: '127.0.0.1'
 }
 
-
-exports = config
+module.exports = config
--- index.mjs
+++ index.mjs
@@ -1,8 +1,28 @@
-import config from "./config.js";
 import Koa from 'koa';
+import fs from 'fs'
+import process from 'process'
+import config from "./config.js"
 
-console.log(config)
+function is_listen_path() {
+  return (typeof config.path === 'string' || config.path instanceof String)
+}
+
+// Remove socket file if exists.
+function remove_socket_file() {
+  if (is_listen_path()) {
+    if (fs.existsSync(config.path))
+      fs.unlinkSync(config.path)
+  }
+}
+
 
 const app = new Koa()
 
-app.listen(config.listen)
+
+if (is_listen_path()) {
+  process.on("exit", remove_socket_file)
+  remove_socket_file()
+  app.listen(config.path)
+}
+else
+  app.listen(config.port || 3000, config.host || '127.0.0.1')
