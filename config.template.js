1ed7cd45391c3a6be23061d1559c725dd7182ad6
Whale Mo <ncwhale@gmail.com>
2020-10-01 11:09:03 +0800

    Add debug flag & add logger/entrypoint detect.

--- config.template.js
+++ config.template.js
@@ -1,8 +1,9 @@
 const config = {
     bot_token: "PUT_YOUR_BOT_TOKEN_HERE",
+    debug: false,             // If you want some logs
     path: "./path/to/socket", // Use path for IPC listen
     port: 3000,               // Or use port [host]
-    host: '127.0.0.1'
+    host: '127.0.0.1'         // Warning: path will override port[host] param.
 }
 
 module.exports = config
--- index.mjs
+++ index.mjs
@@ -1,12 +1,22 @@
 import Koa from 'koa'
 import KoaJson from 'koa-json'
-import { router } from './route/index.mjs'
+import KoaLogger from 'koa-logger'
+import router from './route/index.mjs'
 import fs from 'fs'
 import process from 'process'
 import config from "./config.js"
 
 function is_listen_path() {
   return (typeof config.path === 'string' || config.path instanceof String)
+}
+
+// Wait for import.meta.main show in node.
+function is_entrypoint() {
+  if ('main' in import.meta) {
+    return import.meta.main
+  }
+
+  return true;
 }
 
 // Remove socket file if exists.
@@ -17,18 +27,26 @@ function remove_socket_file() {
   }
 }
 
-
 const app = new Koa()
+
+// Debugger logger
+if ('debug' in config && config.debug) {
+  app.use(KoaLogger())
+}
 
 // Koa middleware
 app.use(KoaJson(config.json || { pretty: false, param: "p" }))
   .use(router.routes())
   .use(router.allowedMethods())
 
-if (is_listen_path()) {
-  process.on("exit", remove_socket_file)
-  remove_socket_file()
-  app.listen(config.path)
+if (is_entrypoint()) {
+  if (is_listen_path()) {
+    process.on("exit", remove_socket_file)
+    remove_socket_file()
+    app.listen(config.path)
+  }
+  else
+    app.listen(config.port || 3000, config.host || '127.0.0.1')
 }
-else
-  app.listen(config.port || 3000, config.host || '127.0.0.1')
+
+export default app
