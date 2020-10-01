44c23cecc2f956e8f7693239b3769160d2aba9c8
Whale Mo <ncwhale@gmail.com>
2020-10-01 17:04:03 +0800

    Use token path for bot webhook.

--- route/index.mjs
+++ route/index.mjs
@@ -4,10 +4,9 @@ import Bot from './bot.mjs'
 let router = new KoaRouter()
 
 router
-  .use('/bot', Bot.routes(), Bot.allowedMethods())
-  .all('/', (ctx, next) => {
-    ctx.body = ''
-    ctx.state = 204
+  .use('/bot/:token', Bot.routes(), Bot.allowedMethods())
+  .all('/(.*)', (ctx, next) => {
+    ctx.body = null
   })
 
 export default router
