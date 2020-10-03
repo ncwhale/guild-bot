c98055eecd5b92a54f0c323bcab1321a8f3f8043
Whale Mo <ncwhale@gmail.com>
2020-10-04 03:50:10 +0800

    Make logger fast again.

--- middleware/logger.mjs
+++ middleware/logger.mjs
@@ -1,37 +1,57 @@
 function create_logger(config) {
-  let cfg = Object.assign({ verbose: 0 }, config)
+  var debug = config.debug || cfg.verbose > 4
+  var log = config.log
 
-  var request_id = 0;
-  var log = config.log;
-  if (cfg.verbose > 3) {
+  if (debug) {
+    // debug logger
+    var request_id = 0
     log = log.child({ module: 'koa' })
-  }
+    return function (ctx, next) {
+      const ctx_log = ctx.log = log.child({
+        ReqId: ++request_id,
+      });
 
-  return function (ctx, next) {
-    let ctx_log = ctx.log = log.child({
-      ReqId: ++request_id,
-    });
+      // Debug path, TMI! TMI!
+      const start = Date.now()
+      ctx_log.debug({
+        request: ctx.request
+      })
 
-    let start
-    if (cfg.debug) {
-      start = Date.now()
-    }
-
-    next()
-      .then((ctx) => {
-        if (cfg.debug) {
+      return next()
+        .then(() => {
           const ms = Date.now() - start
-          ctx_log.debug({ ms }, "Request Done")
-        } else {
-          ctx_log.debug("Request Done")
-        }
-      })
-      .catch((err) => {
+          ctx_log.debug({
+            ms,
+            response: ctx.response
+          }, "Request Done")
+        })
+        .catch((err) => {
+          ctx_log.error(err)
+          throw err
+        })
+    }
+  } else if (config.slient) {
+    // Just inject log item, nothing else.
+    return function (ctx, next) {
+      ctx.log = log
+      return next()
+    }
+  } else {
+    // Normal logger
+    return function (ctx, next) {
+      var request_id = 0
+
+      const ctx_log = ctx.log = log.child({
+        ReqId: ++request_id,
+      });
+
+      return next().catch((err) => {
         ctx_log.error(err)
-        // err.expose = true
-        // throw err
+        err.expose = true
+        throw err
       })
-  }
+    }
+  } // Normal logger
 }
 
 export default create_logger
