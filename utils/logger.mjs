487f51d4c11ed33048a61400d003ada159e64820
Whale Mo <ncwhale@gmail.com>
2020-10-04 03:48:41 +0800

    Add new slient config level for logger.

--- utils/logger.mjs
+++ utils/logger.mjs
@@ -1,35 +1,37 @@
 import pino from 'pino'
 
 function create_logger(config) {
-  let log_config = Object.assign({}, config.log)
+  let pino_config = Object.assign({}, config.log)
 
-  if (config.debug) {
-    log_config.level = 'debug'
-  }
-  
   if (config.verbose) {
     switch (config.verbose) {
       case 5:
-        log_config.level = 'debug'
+        pino_config.level = 'debug'
       case 4:
-        log_config.level = 'info'
+        pino_config.level = 'info'
       case 3:
-        log_config.level = 'warn'
+        pino_config.level = 'warn'
       case 2:
-        log_config.level = 'error'
+        pino_config.level = 'error'
       case 1:
-        log_config.level = 'fatal'
+        pino_config.level = 'fatal'
       case 0:
-        log_config.level = 'slient'
+        pino_config.level = 'slient'
       default:
         // Almost everything.
-        log_config.level = 'trace'
+        pino_config.level = 'trace'
     }
-  // } else {
-  //   log_config.level = 'silent'
   }
 
-  return pino(log_config)
+  if (config.debug) {
+    pino_config.level = 'debug'
+  }
+
+  if (config.slient) {
+    pino_config.level = 'silent'
+  }
+
+  return pino(pino_config)
 }
 
 export default create_logger
