4dbedcc62b5fede28ce48a5c3a0b9be331a10c6c
Whale Mo <ncwhale@gmail.com>
2020-10-04 03:47:11 +0800

    Merge more config varbs.

--- utils/config.mjs
+++ utils/config.mjs
@@ -1,8 +1,12 @@
 import argv from './argv.js'
 import logger from './logger.mjs'
 
-let config = Object.assign({}, argv, {
-  log: logger(argv)
-})
+let config = Object.assign({
+  verbose: 2,     //Default level: error
+  slient: false,  //Default: not slient.
+  debug: false    //Default: not debug
+}, argv)
+
+config.log = logger(config)
 
 export default config
