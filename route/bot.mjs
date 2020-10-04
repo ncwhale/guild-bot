4ccbf395a151babe0b6d6cf5494564311d99f625
Whale Mo <ncwhale@gmail.com>
2020-10-04 15:11:13 +0800

    Await bot update call.

--- route/bot.mjs
+++ route/bot.mjs
@@ -4,14 +4,14 @@ import KoaBody from 'koa-body'
 let router = new KoaRouter()
 
 router
-  .post('/:callback_token', KoaBody(), (ctx) => {
+  .post('/:callback_token', KoaBody(), async (ctx) => {
     if (ctx.params.callback_token !== ctx.bot.callback_hash) {
       ctx.log.error({ params: ctx.params, body: ctx.request.body }, "Callback token not match")
       ctx.throw(405)
       return
     }
 
-    ctx.body = ctx.bot.update(ctx, ctx.request.body)
+    await ctx.bot.update(ctx, ctx.request.body)
   })
 
 export default router
