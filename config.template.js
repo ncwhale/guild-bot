58a4b88b09fca6f5f19d85026aa7531337650698
Whale Mo <ncwhale@gmail.com>
2020-10-04 09:03:54 +0800

    Update config template.

--- config.template.js
+++ config.template.js
@@ -1,9 +1,17 @@
 const config = {
-    bot_token: "PUT_YOUR_BOT_TOKEN_HERE",
-    debug: false,             // If you want some logs
-    path: "./path/to/socket", // Use path for IPC listen
-    port: 3000,               // Or use port [host]
-    host: '127.0.0.1'         // Warning: path will override port[host] param.
+    debug: false,             // If you want more(toooo much) logs
+    slient: false,            // If you want the best performance
+    verbose: 3,               // [1..6]: 'fatal', 'error', 'warn', 'info', 'debug', 'trace'
+    bot: {
+        token: "PUT_YOUR_BOT_TOKEN_HERE",
+        prefix: "https://YOUR_BOT_SERVICE_URL/"
+    },
+    log: {},                  // Logger options for [pino](https://github.com/pinojs/pino/blob/master/docs/api.md#options)
+    service: {
+        path: "./path/to/socket", // Use path for IPC listen
+        port: 3000,               // Or use port [host]
+        host: '127.0.0.1'         // Warning: path will override port[host] param.
+    }
 }
 
 module.exports = config
