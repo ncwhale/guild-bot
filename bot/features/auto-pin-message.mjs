a32bf8b37b88cbf3a09a8646d7c41be6505309e1
Whale Mo <ncwhale@gmail.com>
2020-10-04 19:27:16 +0800

    Add auto pin message feature.

--- bot/features/auto-pin-message.mjs
+++ bot/features/auto-pin-message.mjs
@@ -1,9 +1,18 @@
 class GuildBattlePin {
   constructor(config){
+    this.regex = config.auto_pin_regex || /(?:[Bb][12345]|[一二三四五壹貳叄肆伍]王|挂树)/
   }
 
   message(ctx, update){
-    
+    if ('text' in update.message
+    && !update.message.from.is_bot
+    && this.regex.test(update.message.text)){
+      return ctx.bot.pinChatMessage({
+        chat_id: update.message.chat.id,
+        message_id: update.message.message_id,
+        disable_notification: true,
+      })
+    }
   }
 }
 
