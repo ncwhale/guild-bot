fc30f05ac70e186087366c667407b37c1b9bb37f
Whale Mo <ncwhale@gmail.com>
2020-10-11 10:23:50 +0800

    Add runtime models loader.

--- model/runtime.mjs
+++ model/runtime.mjs
@@ -1,7 +1,11 @@
-
+import GuildInfo from './runtime/guild-info.mjs'
+import GuildPlayer from './runtime/guild-player.mjs'
+import GuildBattle from './runtime/guild-battle.mjs'
 
 function define_model(sequelize){
-
+  GuildInfo(sequelize)
+  GuildPlayer(sequelize)
+  GuildBattle(sequelize)
 }
 
 export default define_model
