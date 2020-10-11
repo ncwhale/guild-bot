024a132b01cde4a6c609ab1d46713d1bdc7c0703
Whale Mo <ncwhale@gmail.com>
2020-10-11 08:43:07 +0800

    Add commands for Guild Battle Feature.

--- bot/features/guild-battle-prgress.mjs
+++ bot/features/guild-battle-prgress.mjs
@@ -2,6 +2,16 @@ class GuildBattleProgress {
   constructor(config){
   }
 
+  commands(){
+    return [{
+      command: "register",
+      description: "注册成为骑空士",
+    }, {
+      command: "unregister",
+      description: "变成古战场逃兵"
+    }]
+  }
+
   message(ctx, update){
   }
 }
